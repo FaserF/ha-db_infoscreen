b453a55a727c6100fcf1928259698fbd4147e49b
Jaegerstefan <34920124+Jaegerstefan@users.noreply.github.com>
2025-10-14 21:32:03 +0200

    Update const.py
    Added RVV

--- custom_components/db_infoscreen/const.py
+++ custom_components/db_infoscreen/const.py
@@ -28,7 +28,7 @@ CONF_DEDUPLICATE_DEPARTURES = "deduplicate_departures"
 DATA_SOURCE_OPTIONS = [
     "IRIS-TTS", "hafas=1", "AVV (Aachen)", "AVV (Augsburg)", "BART", "BEG", "BLS", "BSVG", "BVG", "bwegt", "CMTA", "DING", "DSB", "IE",
     "KVV", "KVB", "LinzAG", "mobiliteit", "MVV", "NAHSH", "NASA", "NVBW", "NVV", "NWL", "\u00d6BB", "PKP", "Resrobot", "RMV", "Rolph",
-    "RSAG", "SaarVV", "STV", "TPG", "VAG", "VBB", "VBN", "VGN", "VMT", "VMV", "VOS", "VRN", "VRN2", "VRR",
+    "RSAG", "RVV", "SaarVV", "STV", "TPG", "VAG", "VBB", "VBN", "VGN", "VMT", "VMV", "VOS", "VRN", "VRN2", "VRR",
     "VRR2", "VRR3", "VVO", "VVS", "ZVV"
 ]
 
