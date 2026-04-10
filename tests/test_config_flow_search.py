c8e6c3fa551ee0720c56ec3b23784c3a727c863e
Fabian Seitz <seitzf1@yahoo.de>
2026-04-10 21:12:13 +0200

    Update test_config_flow_search.py

--- tests/test_config_flow_search.py
+++ tests/test_config_flow_search.py
@@ -205,4 +205,7 @@ async def test_manual_entry(hass: HomeAssistant) -> None:
         assert result["type"] == "create_entry"
         assert result["title"] == "MyCustomStation"
         assert result["data"][CONF_STATION] == "MyCustomStation"
-        assert result["data"][CONF_DATA_SOURCE] == "MVV – Münchener Verkehrs- und Tarifverbund"
+        assert (
+            result["data"][CONF_DATA_SOURCE]
+            == "MVV – Münchener Verkehrs- und Tarifverbund"
+        )
